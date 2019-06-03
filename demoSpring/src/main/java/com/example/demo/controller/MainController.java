6e34894d1f8c06fce5159001b34f7746907fe131
thanhpv <phamthanh.ptit@gmail.com>
2019-06-03 17:15:23 +0700

    update

--- demoSpring/src/main/java/com/example/demo/controller/MainController.java
+++ demoSpring/src/main/java/com/example/demo/controller/MainController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,27 +11,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.security.Principal;
 
-@Controller
-public class MainController {
+@Controller public class MainController {
 
-
-    @RequestMapping(value = "/admin", method = RequestMethod.GET)
-    public String adminPage(Model model, Principal principal) {
+    @RequestMapping(value = "/admin", method = RequestMethod.GET) public String adminPage(Model model,
+            Principal principal) {
         return "adminPage";
     }
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginPage(Model model) {
+    @RequestMapping(value = "/login", method = RequestMethod.GET) public String loginPage(Model model) {
         return "loginPage";
     }
 
-    @RequestMapping(value = "/userInfo", method = RequestMethod.GET)
-    public String userInfo(Model model, Principal principal) {
-        return "userInfoPage";
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home() {
+        return "page/index";
     }
 
-    @RequestMapping(value = "/403", method = RequestMethod.GET)
-    public String accessDenied(Model model, Principal principal) {
+    @RequestMapping(value = "/403", method = RequestMethod.GET) public String accessDenied(Model model,
+            Principal principal) {
 
         if (principal != null) {
             String message = "Hi " + principal.getName() //
@@ -39,8 +40,13 @@ public class MainController {
         return "403Page";
     }
 
-    @RequestMapping(value = "/signUpPage", method = RequestMethod.GET)
-    public String signPage(Model model) {
+    @RequestMapping(value = "/signUpPage", method = RequestMethod.GET) public String signPage(Model model) {
         return "signUpPage";
+    }
+
+    @MessageMapping(value = "/chat")
+    @SendTo("topic/messages")
+    public Message send(@Payload Message message) {
+        return message;
     }
 }
---
+++ demoSpring/src/main/java/com/example/demo/dto/Message.java
@@ -0,0 +1,22 @@
+package com.example.demo.dto;
+
+public class Message {
+    private String userName;
+    private String text;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
